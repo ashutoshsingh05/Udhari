e97d664e0f8035ef7162adf2cc08fd0db348d82a
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2019-06-06 14:20:27 +0530

    Replaced bottomAppBar with bottomNavigationBar :sparkles:

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,18 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:udhari_2/Screens/Forms/ExpensesForm.dart';
 import 'package:udhari_2/Screens/Forms/NormalUdhariForm.dart';
 import 'package:udhari_2/Screens/Forms/TripsForm.dart';
-import 'package:udhari_2/Screens/HomePages/Dashboard.dart';
-import 'package:udhari_2/Screens/HomePages/Trips.dart';
-import 'package:udhari_2/Screens/HomePages/History.dart';
-import 'package:udhari_2/Screens/HomePages/NormalUdhari.dart';
+import 'package:udhari_2/Screens/HomePageScreens/Dashboard.dart';
 import 'package:udhari_2/Utils/ScreenHandler.dart';
-import 'package:udhari_2/Utils/IconHandler.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import 'package:udhari_2/Widgets/Layout.dart';
-import 'package:udhari_2/Widgets/FabWithIcons.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({@required this.user});
@@ -25,26 +20,19 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   ScreenHandler screens;
-  IconHandler homeIcon =
-      IconHandler(Icon(Icons.home, color: Colors.blueAccent));
-  IconHandler personIcon = IconHandler(Icon(Icons.person));
-  IconHandler peopleIcon = IconHandler(Icon(Icons.group));
-  IconHandler historyIcon = IconHandler(Icon(Icons.history));
+
+  int _iconIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    screens = ScreenHandler(Dashboard(user: widget.user));
+    screens = ScreenHandler(Dashboard(user: widget.user), widget.user);
   }
 
   @override
   void dispose() {
     super.dispose();
     screens.screenController.close();
-    homeIcon.iconController.close();
-    personIcon.iconController.close();
-    peopleIcon.iconController.close();
-    historyIcon.iconController.close();
   }
 
   @override
@@ -76,88 +64,31 @@ class _HomePageState extends State<HomePage> {
           return snapshot.data;
         },
       ),
-      bottomNavigationBar: BottomAppBar(
-        elevation: 5,
-        shape: CircularNotchedRectangle(),
-        notchMargin: 4.0,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            IconButton(
-              icon: StreamBuilder(
-                stream: homeIcon.iconStream,
-                initialData: Container(),
-                builder: (BuildContext context, snapshot) {
-                  return snapshot.data;
-                },
-              ),
-              onPressed: () {
-                screens.screenSink.add(Dashboard(user: widget.user));
-                homeIcon.changeIcon(Icon(Icons.home, color: Colors.blueAccent));
-                personIcon.changeIcon(Icon(Icons.person, color: Colors.black));
-                peopleIcon.changeIcon(Icon(Icons.people, color: Colors.black));
-                historyIcon
-                    .changeIcon(Icon(Icons.history, color: Colors.black));
-              },
-            ),
-            IconButton(
-              icon: StreamBuilder(
-                stream: personIcon.iconStream,
-                initialData: Container(),
-                builder: (BuildContext context, snapshot) {
-                  return snapshot.data;
-                },
-              ),
-              onPressed: () {
-                screens.screenSink.add(NormalUdhari(user: widget.user));
-                homeIcon.changeIcon(Icon(Icons.home, color: Colors.black));
-                personIcon
-                    .changeIcon(Icon(Icons.person, color: Colors.blueAccent));
-                peopleIcon.changeIcon(Icon(Icons.people, color: Colors.black));
-                historyIcon
-                    .changeIcon(Icon(Icons.history, color: Colors.black));
-              },
-            ),
-            // SizedBox(
-            //   width: 50,
-            // ),
-            IconButton(
-              icon: StreamBuilder(
-                stream: peopleIcon.iconStream,
-                initialData: Container(),
-                builder: (BuildContext context, snapshot) {
-                  return snapshot.data;
-                },
-              ),
-              onPressed: () {
-                screens.screenSink.add(Trips(user: widget.user));
-                homeIcon.changeIcon(Icon(Icons.home, color: Colors.black));
-                personIcon.changeIcon(Icon(Icons.person, color: Colors.black));
-                peopleIcon
-                    .changeIcon(Icon(Icons.people, color: Colors.blueAccent));
-                historyIcon
-                    .changeIcon(Icon(Icons.history, color: Colors.black));
-              },
-            ),
-            IconButton(
-              icon: StreamBuilder(
-                stream: historyIcon.iconStream,
-                initialData: Container(),
-                builder: (BuildContext context, snapshot) {
-                  return snapshot.data;
-                },
-              ),
-              onPressed: () {
-                screens.screenSink.add(History(user: widget.user));
-                homeIcon.changeIcon(Icon(Icons.home, color: Colors.black));
-                personIcon.changeIcon(Icon(Icons.person, color: Colors.black));
-                peopleIcon.changeIcon(Icon(Icons.people, color: Colors.black));
-                historyIcon
-                    .changeIcon(Icon(Icons.history, color: Colors.blueAccent));
-              },
-            ),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.blue,
+        type: BottomNavigationBarType.fixed,
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+        currentIndex: _iconIndex,
+        onTap: _handleBottomBarOnTap,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            title: Text("Home"),
+            icon: Icon(Icons.home),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Udhari"),
+            icon: Icon(Icons.person),
+          ),
+          BottomNavigationBarItem(
+            title: Text("Trips"),
+            icon: Icon(Icons.group),
+          ),
+          BottomNavigationBarItem(
+            title: Text("History"),
+            icon: Icon(Icons.history),
+          ),
+        ],
       ),
       floatingActionButton: SpeedDial(
         marginRight: 18,
@@ -184,6 +115,7 @@ class _HomePageState extends State<HomePage> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
+                    fullscreenDialog: true,
                     builder: (BuildContext context) {
                       return ExpensesForm(user: widget.user);
                     },
@@ -199,6 +131,7 @@ class _HomePageState extends State<HomePage> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
+                  fullscreenDialog: true,
                   builder: (BuildContext context) {
                     return NormalUdhariForm(user: widget.user);
                   },
@@ -215,6 +148,7 @@ class _HomePageState extends State<HomePage> {
               Navigator.push(
                 context,
                 MaterialPageRoute(
+                  fullscreenDialog: true,
                   builder: (BuildContext context) {
                     return TripsForm(user: widget.user);
                   },
@@ -225,6 +159,13 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  _handleBottomBarOnTap(int index) {
+    screens.setScreen = index;
+    setState(() {
+      _iconIndex = index;
+    });
   }
 
   void _signOut() async {
--- lib/Utils/ScreenHandler.dart
+++ lib/Utils/ScreenHandler.dart
@@ -1,18 +1,42 @@
 import 'dart:async';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:udhari_2/Screens/HomePageScreens/Dashboard.dart';
+import 'package:udhari_2/Screens/HomePageScreens/History.dart';
+import 'package:udhari_2/Screens/HomePageScreens/NormalUdhari.dart';
+import 'package:udhari_2/Screens/HomePageScreens/Trips.dart';
 
 class ScreenHandler {
-  ScreenHandler(Widget initialScreen) {
+  ScreenHandler(Widget initialScreen, FirebaseUser user) {
     this.screen = initialScreen;
+    this.user = user;
     changeScreen(this.screen);
   }
 
   Widget screen;
+  FirebaseUser user;
   StreamController screenController = StreamController<Widget>();
   Sink get screenSink => screenController.sink;
   Stream<Widget> get screenStream => screenController.stream;
 
-  changeScreen(Widget newWidget) {
-    screenSink.add(newWidget);
+  void changeScreen(Widget newScreen) {
+    screenSink.add(newScreen);
+  }
+
+  set setScreen(int index) {
+    switch (index) {
+      case 0:
+        changeScreen(Dashboard(user: this.user));
+        break;
+      case 1:
+        changeScreen(NormalUdhari(user: this.user));
+        break;
+      case 2:
+        changeScreen(Trips(user: this.user));
+        break;
+      case 3:
+        changeScreen(History(user: this.user));
+        break;
+    }
   }
 }
